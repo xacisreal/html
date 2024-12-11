d9c4c04330b5fb374e54efca64df662043c0931a
xacisreal <yoimretard@gmail.com>
2024-12-11 10:34:16 +0530

    Add files via upload

---
+++ php/lab21.php
@@ -0,0 +1,9 @@
+<?php
+$a = array("Anjala", "Christeena", "Blessey","Angela"); 
+print_r($a);
+echo("<br>Ascending order : "); 
+asort($a);
+print_r($a); echo("<br>Descending order : "); 
+arsort($a);
+print_r($a);
+?>
